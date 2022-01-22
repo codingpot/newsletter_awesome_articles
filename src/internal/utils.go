bb337ae7fa42c5224477cb6e0f5f916a9d1c44bf
Chansung Park <deep.diver.csp@gmail.com>
2022-01-22 13:36:10 +0000

    write utility functions

---
+++ src/internal/utils.go
@@ -0,0 +1,53 @@
+package internal
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func GetListOfFilesAt(in string, extension string) []string {
+	in, _ = filepath.Abs(in)
+
+	filenames := []string{}
+	files, _ := ioutil.ReadDir(in)
+
+	for _, f := range files {
+		if filepath.Ext(f.Name()) == extension {
+			filenames = append(filenames, fmt.Sprintf("%s/%s", in, f.Name()))
+		}
+	}
+
+	return filenames
+}
+
+func ZipArticleTuples(articles []Article) []ArticleTuple {
+	articleTuples := []ArticleTuple{}
+
+	if len(articles) == 1 {
+		articleTuples = append(articleTuples,
+			ArticleTuple{
+				Article1: articles[0],
+				Article2: Article{},
+			})
+		return articleTuples
+	}
+
+	for i := 0; i < len(articles); i += 2 {
+		if i+1 == len(articles) {
+			articleTuples = append(articleTuples,
+				ArticleTuple{
+					Article1: articles[i],
+					Article2: Article{},
+				})
+		} else {
+			articleTuples = append(articleTuples,
+				ArticleTuple{
+					Article1: articles[i],
+					Article2: articles[i+1],
+				})
+		}
+	}
+
+	return articleTuples
+}
